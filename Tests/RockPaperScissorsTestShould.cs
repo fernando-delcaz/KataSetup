dbed48dd4d9fc79600a424d47cdaa6bed14716e4
fdcaz <fdcaz@voxelgroup.net>
2024-10-28 14:47:12 +0100

    refactor: extract setup

--- Tests/RockPaperScissorsTestShould.cs
+++ Tests/RockPaperScissorsTestShould.cs
@@ -4,14 +4,20 @@ namespace Tests;
 
 public class RockPaperScissorsTestShould
 {
+    private readonly Game _game;
+    private const string PlayerOne = "Manu";
+    private const string PlayerTwo = "Fer";
+
+    public RockPaperScissorsTestShould()
+    {
+        var game = new Game(PlayerOne, PlayerTwo);
+        _game = game;
+    }
+
     [Fact]
     public void PlayerOneWithRockBeatsPlayerTwoWithScissors()
     {
-        var playerOne = "Manu";
-        var playerTwo = "Fer";
-        var game = new Game(playerOne, playerTwo);
-
-        var result = game.play("ROCK", "SCISSORS");
+        var result = _game.play("ROCK", "SCISSORS");
 
         Assert.Equal("ROCK", result);
     }
@@ -19,24 +25,14 @@ public class RockPaperScissorsTestShould
     [Fact]
     public void PlayerOneWithPaperBeatsPlayerTwoWithRock()
     {
-        var playerOne = "Manu";
-        var playerTwo = "Fer";
-        var game = new Game(playerOne, playerTwo);
-
-        var result = game.play("PAPER", "ROCK");
+        var result = _game.play("PAPER", "ROCK");
 
         Assert.Equal("PAPER", result);
     }
 
-
     [Fact]
     public void PlayerOneWithScissorsIsBeatenByPlayerTwoWithRock()
-    {
-        var playerOne = "Manu";
-        var playerTwo = "Fer";
-        var game = new Game(playerOne, playerTwo);
-
-        var result = game.play("SCISSORS", "ROCK");
+    { var result = _game.play("SCISSORS", "ROCK");
 
         Assert.Equal("ROCK", result);
     }
