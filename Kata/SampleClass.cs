3ce16d10eb627b631914773d4b8fd8af242980fd
fdcaz <fdcaz@voxelgroup.net>
2024-10-28 13:48:34 +0100

    chore: initial commit

---
+++ Kata/SampleClass.cs
@@ -0,0 +1,9 @@
+namespace Kata;
+
+public class SampleClass
+{
+    public SampleClass()
+    {
+        
+    }
+}
---
+++ Tests/SampleUnitTest.cs
@@ -0,0 +1,12 @@
+using Kata;
+
+namespace Tests;
+
+public class SampleUnitTest
+{
+    [Fact]
+    public void SampleTest()
+    {
+        Assert.NotNull(new SampleClass());
+    }
+}
